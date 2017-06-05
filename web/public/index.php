5863eb415700ea79ffd9b3ddcedd925720862d9e
CAIO SANSHES <caiobentes@ufpa.br>
2017-06-05 11:43:51 -0300

    commit

--- web/public/index.php
+++ web/public/index.php
@@ -1,15 +1,82 @@
 <?php
+    session_start();
+    
 
-include '../app/vendor/autoload.php';
-$foo = new App\Acme\Foo();
+    if(isset($_SESSION['usuarioNiveisAcesso'])){
 
-?><!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Docker <?php echo $foo->getName(); ?></title>
-    </head>
-    <body>
-        <h1>Docker <?php echo $foo->getName(); ?></h1>
-    </body>
+        if ($_SESSION['usuarioNiveisAcesso'] == "1") {
+            header("Location: administrativo.php");
+        }elseif($_SESSION['usuarioNiveisAcesso'] == "0")
+        {   header("Location: colaborador.php");}
+    }
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="Sistema de login Setsetver">
+    <meta name="author" content="Jefferson Palheta">
+    <link rel="icon" href="sf.png">
+
+    <title>SetFinger</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/signin.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    
+    <script src="js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container">
+    <img src="logo.png" alt="..." class="img-responsive center-block" width="110px"/>
+      <form class="form-signin" method="POST" action="valida.php">
+        <h2 class="form-signin-heading text-center">SetFinger</h2>
+        
+        <label for="usuario" class="sr-only">Usuario</label>
+        <input type="text" name="usuario" class="form-control" placeholder="Usuario" required autofocus>
+        
+        <label for="senha" class="sr-only">Senha</label></br>
+        <input type="password" name="senha" class="form-control" placeholder="Senha" required>
+        
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
+      </form>
+      <p class="text-center text-danger">
+            <?php if(isset($_SESSION['loginErro'])){
+                echo $_SESSION['loginErro'];
+                unset($_SESSION['loginErro']);
+            }?>
+        </p>
+        <p class="text-center text-success">
+            <?php 
+            if(isset($_SESSION['logindeslogado'])){
+                echo $_SESSION['logindeslogado'];
+                unset($_SESSION['logindeslogado']);
+            }
+            ?>
+        </p>
+    </div> <!-- /container -->
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+  </body>
 </html>
